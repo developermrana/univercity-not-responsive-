9b78d027ee62fa1e95c4bf29279c9b15746edc0c
Masud Rana <119274830+mrana565@users.noreply.github.com>
2023-05-29 11:20:49 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+var navLink = document.getElementsById("navLink");
+
+function showManu(){
+    navLink.style.right = "0";
+}
+function hideManu(){
+    navLink.style.right = "-200px";
+}
+ /* header section end */
+
+
+/* serves section start */
+
+
+
+
+/* serves section end */
